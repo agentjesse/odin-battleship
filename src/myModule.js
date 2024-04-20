ae7bb8e374c49118025fab92618498feda46cc80
Jesse TR <31576321+agentjesse@users.noreply.github.com>
2024-04-20 04:50:17 -0400

    Initial commit

---
+++ src/myModule.js
@@ -0,0 +1,3 @@
+export const functionOne = () => console.log('FUNCTION ONE LOGGED!'); //inline named export
+// export { functionOne }; //separate named export example
+// export default functionOne; //non-named export example
